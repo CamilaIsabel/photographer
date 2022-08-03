3cb9a1ecd678dda221f38d191cf2c0168156aeda
CamilaIsabel <camilazubillaga123@gmail.com>
2022-08-03 12:07:47 -0500

    about me

---
+++ src/app/components/about/about-me.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AboutMeComponent } from './about-me';
+
+@NgModule({
+  declarations: [
+    AboutMeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+})
+export class AboutMeModule { }
