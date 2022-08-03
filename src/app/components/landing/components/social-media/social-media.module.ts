215e05c375d396a65ab7a737921735d68017e77a
CamilaIsabel <camilazubillaga123@gmail.com>
2022-08-02 23:15:35 -0500

    tailwind social media

---
+++ src/app/components/landing/components/social-media/social-media.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { SocialMediaComponent } from './social-media.component';
+
+@NgModule({
+  declarations: [
+    SocialMediaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+})
+export class SocialMediaModule { }
--- src/app/components/landing/landing.module.ts
+++ src/app/components/landing/landing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LandingComponent } from './landing.component';
 import { TopHeaderComponent } from './components/header/top-header.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
     LandingComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
